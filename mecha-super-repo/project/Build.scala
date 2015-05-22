5bd7b74880dbc6f620e76cabb47a1beab32c94a1
Aleksandar Prokopec <aleksandar.prokopec@gmail.com>
2015-05-23 01:47:22 +0200

    Add mecha-super-repo example.

---
+++ mecha-super-repo/project/Build.scala
@@ -0,0 +1,27 @@
+
+
+
+import org.stormenroute.mecha._
+import sbt._
+import sbt.Keys._
+
+
+
+object MechaSuperRepoBuild extends MechaSuperBuild {
+
+  lazy val mechaSuperRepoSettings = Defaults.defaultSettings ++
+    defaultMechaSuperSettings ++ Seq(
+    name := superName,
+    scalaVersion := "2.11.4",
+    version := "0.1",
+    organization := "com.storm-enroute",
+    libraryDependencies ++= Seq()
+  )
+
+  val superName = "super-storm-enroute"
+
+  val superDirectory = file(".")
+
+  val superSettings = mechaSuperRepoSettings
+
+}
