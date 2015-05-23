3afc8297398a074456560b0f06542892447ada8f
Aleksandar Prokopec <aleksandar.prokopec@gmail.com>
2015-05-23 20:43:48 +0200

    Add script for building example projects.

---
+++ build-all.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+if [ $# -ne 1 ]; then
+  echo "Please specify the SBT command as the first argument, exiting."
+  exit 1
+fi
+
+CURR_DIR=`pwd`
+SCRIPT_PATH=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+SBT_CMD="${1}"
+
+function test() {
+  declare path=$1
+  declare commands=$2
+  echo "Building $path with commands: $commands"
+  cd $SCRIPT_PATH/$path
+  $SBT_CMD $commands
+}
+
+test mecha-super-repo "clean compile"
+
+cd $CURR_DIR
